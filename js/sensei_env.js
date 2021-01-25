8d41a1567074cb5e681746caace19b6045c4b859
Benjamin Teuber <bsteuber@gmail.com>
2021-01-25 10:48:51 +0100

    Update AI-Sensei

--- js/sensei_env.js
+++ js/sensei_env.js
@@ -1,4 +1,5 @@
 SENSEI_ENV = {
   FIREBASE_DB: "PRODUCTION",
-  SERVER_HOST: "https://us-central1-sensei-160117.cloudfunctions.net/prodServer"
+  SERVER_HOST: "https://us-central1-sensei-160117.cloudfunctions.net/prodServer",
+  WS_LIVE_SERVER_URL: "wss://weiqi-ai.com:8215"
 };

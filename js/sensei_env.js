cc8e513a524507ec0ff6a0d6a43198c8a666c5cc
Benjamin Teuber <bsteuber@gmail.com>
2021-02-12 14:24:15 +0100

    Already enter correct live server url

--- js/sensei_env.js
+++ js/sensei_env.js
@@ -1,5 +1,5 @@
 SENSEI_ENV = {
   FIREBASE_DB: "PRODUCTION",
   SERVER_HOST: "https://us-central1-sensei-160117.cloudfunctions.net/prodServer",
-  WS_LIVE_SERVER_URL: "wss://weiqi-ai.com:8215"
+  WS_SCHEDULER_URL: "wss://weiqi-ai.com"
 };

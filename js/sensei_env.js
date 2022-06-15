27e5d69f65ce686bba8d5a5c3eb9e9f2b051292d
Benjamin Teuber <bsteuber@gmail.com>
2022-06-15 11:22:34 +0200

    Read from firestore now

--- js/sensei_env.js
+++ js/sensei_env.js
@@ -2,5 +2,6 @@ SENSEI_ENV = {
   FIREBASE_DB: "PRODUCTION",
   SERVER_HOST: "https://us-central1-sensei-160117.cloudfunctions.net/prodServer",
   WS_SCHEDULER_URL: "wss://ai-sensei.com/ws",
-  ENABLE_LIVE_MODE: "true"
+  ENABLE_LIVE_MODE: "true",
+  READ_FROM_FIRESTORE_DURING_MIGRATION: "true"
 };

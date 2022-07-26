74f7b4b8923ddc8402f03f5eb9331a7b6c6a1259
Erik Ouchterlony <erik.ouchterlony@bredband.net>
2022-07-26 13:51:48 +0200

    "Update AI-Sensei"

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+// This allows the web app to trigger skipWaiting via
+// registration.waiting.postMessage({type: 'SKIP_WAITING'})
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('install', (event) => {});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(fetch(event.request));
+});

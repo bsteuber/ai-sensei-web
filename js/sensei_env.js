3fc8ac44fab4d8d893e8e513800191e50a5780d8
Benjamin Teuber <bsteuber@gmail.com>
2022-11-08 14:46:22 +0100

    Enable Pandanet sign-in

--- js/sensei_env.js
+++ js/sensei_env.js
@@ -3,5 +3,6 @@ SENSEI_ENV = {
   SERVER_HOST: "https://us-central1-sensei-160117.cloudfunctions.net/prodServer",
   WS_SCHEDULER_URL: "wss://ai-sensei.com/ws",
   ENABLE_LIVE_MODE: "true",
+  ENABLE_PANDANET_SIGNIN: "true",
   READ_FROM_FIRESTORE_DURING_MIGRATION: "true"
 };

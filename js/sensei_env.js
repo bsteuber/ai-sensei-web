62704ddbc5820af6caa87549a78e61ec7d8f506c
Benjamin Teuber <bsteuber@gmail.com>
2023-03-16 10:09:12 +0100

    "Update AI-Sensei"

--- js/sensei_env.js
+++ js/sensei_env.js
@@ -1,4 +1,5 @@
 SENSEI_ENV = {
+  BUILD_STAGE: "prod",
   FIREBASE_DB: "PRODUCTION",
   SERVER_HOST: "https://us-central1-sensei-160117.cloudfunctions.net/prodServer",
   WS_SCHEDULER_URL: "wss://ai-sensei.com/ws",

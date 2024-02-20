0173e9b1b1e7599f2f36b5c5bea25a0e57f16a9e
Benjamin Teuber <bsteuber@gmail.com>
2024-02-20 14:21:05 +0100

    Use old server again

--- js/sensei_env.js
+++ js/sensei_env.js
@@ -1,7 +1,7 @@
 SENSEI_ENV = {
   BUILD_STAGE: "prod",
   FIREBASE_DB: "PRODUCTION",
-  SERVER_HOST: "/api",
+  SERVER_HOST: "https://us-central1-sensei-160117.cloudfunctions.net/prodServer",
   WS_SCHEDULER_URL: "wss://ai-sensei.com/ws",
   ENABLE_LIVE_MODE: "true",
   ENABLE_PANDANET_SIGNIN: "true",
